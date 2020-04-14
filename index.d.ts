2ab0f94f60ebd9c1794c7e5d99867bd9bcfcdf95
Kristian Fallrø <kristfal@me.com>
2020-04-14 17:21:09 +0200

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,11 @@
 declare module 'react-native-simple-compass' {
+  type Callback = {
+    degree: number;
+    accuracy: number;
+  }  
+
   export default class {
-    static start(updateInterval: number, callback: (arg: number) => any): void;
+    static start(updateInterval: number, callback: (arg: Callback) => any): void;
     static stop(): void;
   }
 }
